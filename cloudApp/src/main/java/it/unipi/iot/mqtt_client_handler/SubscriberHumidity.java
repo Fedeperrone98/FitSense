63b1331f388fdc00bfa59b9c2bf561608713cacf
IreneCantini <ire.cantini@icloud.com>
2023-06-15 11:47:55 +0200

    implementation Cloud Application

---
+++ cloudApp/src/main/java/it/unipi/iot/mqtt_client_handler/SubscriberHumidity.java
@@ -0,0 +1,50 @@
+package it.unipi.iot.mqtt_client_handler;
+
+import it.unipi.iot.db_handler.FitSenseDBHandler;
+import org.eclipse.paho.client.mqttv3.*;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
+public class SubscriberHumidity implements MqttCallback {
+
+    String topic = "humidity";
+    String broker = "tcp://127.0.0.1:1883";
+    String clientId = "HumiditySubscriber";
+
+    public SubscriberHumidity() throws MqttException {
+        MqttClient mqttClient = new MqttClient(broker,clientId);
+        mqttClient.setCallback( this );
+        mqttClient.connect();
+        mqttClient.subscribe(topic);
+    }
+
+    @Override
+    public void connectionLost(Throwable throwable) {
+
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage mqttMessage){
+        System.out.println("[!] Receiving humidity message");
+        System.out.println(" <  " + mqttMessage.toString());
+
+        JSONObject genreJsonObject;
+        try {
+            genreJsonObject = (JSONObject) JSONValue.parseWithException(mqttMessage.toString());
+            long value = (long) genreJsonObject.get("value");
+            long area_id = (long) genreJsonObject.get("area_id");
+            long node_id = (long) genreJsonObject.get("node_id");
+
+            FitSenseDBHandler.insertTemperature((int)area_id,(int)node_id,(int)value);
+        }catch (ParseException e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+    }
+}
--- remoteControlApp/src/main/java/it/unipi/iot/db_handler/FitSenseDBHandler.java
+++ remoteControlApp/src/main/java/it/unipi/iot/db_handler/FitSenseDBHandler.java
@@ -65,61 +65,6 @@ public class FitSenseDBHandler {
         return result;
     }
 
-    public static void insertTemperature(int area_id, int node_id, int m_value) {
-        String insertQueryStatement = "INSERT INTO measurement_temperature (area_id, node_id, m_timestamp, m_value) VALUES (?, ?, CURRENT_TIMESTAMP, ?)";
-        try (Connection smartPoolConnection = makeDBConnection()
-        ) {
-            assert smartPoolConnection != null;
-            try (PreparedStatement smartPoolPrepareStat = smartPoolConnection.prepareStatement(insertQueryStatement)
-            ) {
-                smartPoolPrepareStat.setInt(1, area_id);
-                smartPoolPrepareStat.setInt(2, node_id);
-                smartPoolPrepareStat.setInt(3, m_value);
-                smartPoolPrepareStat.executeUpdate();
-
-            }
-        } catch (SQLException sqlEx) {
-            sqlEx.printStackTrace();
-        }
-    }
-
-    public static void insertHumidity(int area_id, int node_id, int m_value) {
-        String insertQueryStatement = "INSERT INTO measurement_humidity (area_id, node_id, m_timestamp, m_value) VALUES (?, ?, CURRENT_TIMESTAMP, ?)";
-        try (Connection smartPoolConnection = makeDBConnection()
-        ) {
-            assert smartPoolConnection != null;
-            try (PreparedStatement smartPoolPrepareStat = smartPoolConnection.prepareStatement(insertQueryStatement)
-            ) {
-                smartPoolPrepareStat.setInt(1, area_id);
-                smartPoolPrepareStat.setInt(2, node_id);
-                smartPoolPrepareStat.setInt(3, m_value);
-                smartPoolPrepareStat.executeUpdate();
-
-            }
-        } catch (SQLException sqlEx) {
-            sqlEx.printStackTrace();
-        }
-    }
-
-    public static void insertPresence(int area_id, int node_id, int m_value) {
-        String insertQueryStatement = "INSERT INTO measurement_presence (area_id, node_id, m_timestamp, m_value) VALUES (?, ?, CURRENT_TIMESTAMP, ?)";
-        try (Connection smartPoolConnection = makeDBConnection()
-        ) {
-            assert smartPoolConnection != null;
-            try (PreparedStatement smartPoolPrepareStat = smartPoolConnection.prepareStatement(insertQueryStatement)
-            ) {
-                smartPoolPrepareStat.setInt(1, area_id);
-                smartPoolPrepareStat.setInt(2, node_id);
-                smartPoolPrepareStat.setInt(3, m_value);
-                smartPoolPrepareStat.executeUpdate();
-
-            }
-        } catch (SQLException sqlEx) {
-            sqlEx.printStackTrace();
-        }
-
-    }
-
     public static void insertConfiguration(int area_id, int node_id, String address) {
         String insertQueryStatement = "INSERT INTO configuration (area_id, node_id, ipv6_address) VALUES (?, ?, ?)";
         try (Connection smartPoolConnection = makeDBConnection()
